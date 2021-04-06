1a61c1ad14380969d64e45a8247f6079c36b2c0d
Julien Bouland <bouland.julien@gmail.com>
2021-04-06 15:21:42 +0200

    Quizz: Active Record validations

--- app/models/quizz.rb
+++ app/models/quizz.rb
@@ -1,2 +1,10 @@
 class Quizz < ApplicationRecord
+  # Active Record validations
+    # `difficulty` has to be one of the options
+  validates :difficulty, inclusion: { in: DIFFICULTY }
+  validates :title, presence: true
+  validates :title, length: { in: 3..30,
+                    too_long: "30 characters is the maximum allowed" }
+
+  DIFFICULTY = ["easy", "medium", "hard"]
 end
