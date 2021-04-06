8ccb0a4d756fda85c581c516972cb9d7851221c5
Julien Bouland <bouland.julien@gmail.com>
2021-04-06 16:35:56 +0200

    Quizz: routes added + Instances variables added to #show & #dashboard

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,20 @@
 class QuizzesController < ApplicationController
+  before_action :set_quizz, only: [:show]
+  
+  def show
+    @questions = Quizz.questions
+    @answers = @questions.answers
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quizz
+    @quizz = Quizz.find(params[:id])
+  end
+
+  # Never trust parameters from user, only allow the white list through.
+  def quizz_params
+    params.require(:quizz).permit(:title, :description, :difficulty)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/dashboard'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :quizz do
+    resources :questions do
+      resources :answers
+    end
+  end
 end
