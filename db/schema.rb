0dc0388fa99358c5333694d9df4ea89482d959d3
Julien Bouland <bouland.julien@gmail.com>
2021-04-06 15:37:19 +0200

    Answer: controller init

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_132551) do
+ActiveRecord::Schema.define(version: 2021_04_06_133253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "title"
+    t.boolean "status"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "title"
@@ -45,5 +54,6 @@ ActiveRecord::Schema.define(version: 2021_04_06_132551) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "quizzs"
 end
