c3a7f97554e1ed598427968bb39fb81a6a069f10
Julien Bouland <bouland.julien@gmail.com>
2021-04-07 12:53:25 +0200

    Rspec: tests passed for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  validates :username, presence: true
+  
+  def email_required?
+    false
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject { described_class.new(username: "Julien", password: "123456",encrypted_password: "123456") }
+
+  describe 'validations' do
+    it 'is valid without an email' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without an username' do
+      subject.username = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid without a password' do
+      subject.password = nil
+      expect(subject).to be_invalid
+    end
+  end
 end
