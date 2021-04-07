8884b5fc67480bb61b9ef5f0bf8117ec5cafe04f
Julien Bouland <bouland.julien@gmail.com>
2021-04-07 13:11:47 +0200

    Rspec: shoulda-matchers added

--- spec/models/quizz_spec.rb
+++ spec/models/quizz_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Quizz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new(title: "Shell quizz.", descritpion: "Welcome to the new shell quizz of quizzator !", difficulty: "hard") }
+
+  describe 'validations' do
+    it 'is invalid without a title' do
+      expect(build(:quiz, title: nil)).to be_invalid
+    end
+  end
 end
